f48117a8f14ca44154248d9ec49cb3a2893b674d
sauravxtha <sauravxtha@gmail.com>
2019-06-14 11:18:42 +0545

    Update main-navbar.php

--- template-parts/navigation/main-navbar.php
+++ template-parts/navigation/main-navbar.php
@@ -11,24 +11,24 @@
 <header id="masthead" class="site-header" role="banner">
 	<nav class="navbar navbar-expand-lg main-navigation">
 		<div class="container">
-			<?php if (!has_custom_logo()) { ?>
+			<?php if ( ! has_custom_logo() ) { ?>
 
-				<?php if (is_front_page() && is_home()) : ?>
+				<?php if ( is_front_page() && is_home() ) : ?>
 
-					<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a></h1>
+					<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
 
 				<?php else : ?>
 
-					<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a>
+					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
 
 				<?php endif; ?>
 
 
-			<?php
-		} else {
-			the_custom_logo();
-		}
-		?>
+				<?php
+			} else {
+				the_custom_logo();
+			}
+			?>
 			<button id="menu" class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#main-nav">
 				<span></span>
 				<span></span>
@@ -43,7 +43,7 @@
 					'container_id'    => 'main-nav',
 					'container_class' => 'collapse navbar-collapse justify-content-end',
 					'menu_id'         => false,
-					'depth'			  => 8,
+					'depth'           => 8,
 					'menu_class'      => 'navbar-nav',
 					'fallback_cb'     => 'Themesmandu_Starter_WP_Bootstrap_Navwalker::fallback',
 					'walker'          => new Themesmandu_Starter_WP_Bootstrap_Navwalker(),
